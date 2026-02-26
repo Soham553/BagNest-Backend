0d795040106c531fa444f6d9e0b6b195b62d1944
Soham553 <sakharesoham10@gmail.com>
2026-02-26 17:00:15 +0530

    Again bugs

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,5 +1,6 @@
 import { v2 as cloudinary } from "cloudinary";
 
+console.log("inside config:",process.env.Api_secret, " ",  process.env.Api_key)
 cloudinary.config({
     cloud_name: process.env.Cloud_Name,
     api_key: process.env.Api_key,
