b5da4e68930457a7db476745f434a655f4536dd5
Soham553 <sakharesoham10@gmail.com>
2026-04-03 23:11:59 +0530

    Add video upload logic on YT using googleapi

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -8,6 +8,9 @@ const productSchema = new mongoose.Schema({
     type: Array,   
     required: true
   },
+  video: {
+    type: Array
+  },
   height: Number,
   width: Number,
   num_of_pockets: Number
